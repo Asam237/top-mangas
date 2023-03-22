6b9e18c54f20a27efa975b3fe3ec992cf6999641
Abba Sali Aboubakar Mamate <abbasalimokolo@gmail.com>
2023-03-22 16:28:32 +0100

    update script

--- utils/yaml-convert.js
+++ utils/yaml-convert.js
@@ -11,20 +11,18 @@ switch (args[0]) {
     convert();
 }
 
-const a = require("../utils/");
-
 function watch() {
-  fs.watch("../assets/data/manga.yaml", function (event, filename) {
-    require("child_process").fork("../utils/yaml-convert.js");
+  fs.watch("./assets/data/manga.yaml", function (event, filename) {
+    require("child_process").fork("./utils/yaml-convert.js");
   });
 }
 
 function convert() {
   try {
     const json = yaml.load(
-      fs.readFileSync("../assets/data/manga.yaml", "utf8")
+      fs.readFileSync("./assets/data/manga.yaml", "utf8")
     );
-    fs.writeFileSync("../assets/data/manga.json", JSON.stringify(json));
+    fs.writeFileSync("./assets/data/manga.json", JSON.stringify(json));
   } catch (e) {
     console.log(e);
   }
